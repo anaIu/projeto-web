8c0b8d63e06f506473de95d452884ff052d280fb
anaIu <analunedo5@gmail.com>
2025-03-26 22:07:13 -0300

    aula 2

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,105 @@
-export default function Home() {
+import Logo from "../assets/logo.svg";
+import "../styles/header.css";
+import "../styles/utility.css";
+import Button from "../components/buttons.tsx"
+import { useState } from "react";
+import Menu from "../assets/menu.svg";
+import Close from "../assets/close.svg";
+// import HeroRectangleOne from "../assets/images/rectangleOne.png";
+// import HeroRectangleTwo from "../assets/images/rectangleTwo.png";
+import "../styles/hero.css";
+
+export default function Header() {
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
     return (
-        <main> 
-            <p>
-                Teste
-            </p>
-        </main>
+        <>
+            <header className="container py-sm">
+                <nav className="flex items-center justify-between">
+                    <img src={Logo} alt="Logo DonaFrost" width={500} height={350} />
+                    <div className="desktop-only">
+                        <ul className="flex gap-1">
+                            <li>
+                                <a href="#">Home</a>
+                            </li>
+                            <li>
+                                <a href="#solution">Soluções</a>
+                            </li>
+                            <li>
+                                <a href="#testimonials">Depoimentos</a>
+                            </li>
+                            <li>
+                                <a href="#pricing">Preços</a>
+                            </li>
+                            <li>
+                                <a href="#contact">Contato</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="desktop-only">
+                        <div className="flex items-center">
+                            <a className="reverse-color ml-lg" href="">Login</a>
+                            <Button text="Cadastre-se" handleFunction={() => console.log("teste")} />
+                        </div>
+                    </div>
+
+                    <div className="mobile-menu">
+                        {showMobileMenu ?
+                            <div className="mobile-menu-content">
+                                <div className="container flex">
+                                    <ul>
+                                        <li>
+                                            <a href="#">Home</a>
+                                        </li>
+                                        <li>
+                                            <a href="#solution">Soluções</a>
+                                        </li>
+                                        <li>
+                                            <a href="#testimonials">Depoimentos</a>
+                                        </li>
+                                        <li>
+                                            <a href="#pricing">Preços</a>
+                                        </li>
+                                        <li>
+                                            <a href="#contact">Contato</a>
+                                        </li>
+                                    </ul>
+                                    <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper">
+                                        <img src={Close} alt="ícone fechar menu" width={24} height={24} />
+                                    </span>
+                                </div>
+                            </div>
+                            :
+                            <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper" >
+                                <img src={Menu} alt="ícone menu" width={24} height={24} />
+                            </span>
+                        }
+                    </div>
+
+
+
+                </nav>
+
+            </header>
+            <section id="hero">
+                <span className="desktop-only">
+                    {/* <img src={HeroRectangleTwo} alt="Retangulo um tela inicial" /> */}
+                </span>
+                {/* <img src={HeroRectangleOne} alt="Retangulo dois tela inicial" /> */}
+                <div className="container content">
+                    <p className="desktop-only">
+                        Olá
+                    </p>
+                    <h1>Comida de mãe direto no seu apê, é só pedir que entregamos para você!</h1>
+                    <p>Já pensou em matar a saudade daquela comida caseira? O melhor de tudo, nossas receitas são 100% saudáveis, bora entrar no shape.
+                    </p>
+                    <div className="flex gap-1">
+                        {/* <span><Button text="Cadastre-se" /></span> */}
+                        <span className="desktop-only">
+                            {/* <Button text="Veja mais" secondary /> */}
+                        </span>
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
